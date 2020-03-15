6e7e838c718e005b87b239e2ed977868433daa45
VoIdJMp <59548686+VoIdJMp@users.noreply.github.com>
2020-03-15 14:52:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const TOKEN = '823493917:AAGZ8k8geDfaWYZPOFffQAZaigwq8eQurcg';
 const bot  = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name} `);
-    bot.sendMessage(msg.chat.id, `Вы гадёныш`);
-    bot.sendMessage(msg.chat.id, `Извыните ${msg.from.first_name}`);
+    if(message.text == 'Привет') {
+        bot.sendMessage(msg.chat.id, `Привет ${msg.from.first_name} `);
+    }
+    if(message.text == 'Как ты?') {
+        bot.sendMessage(msg.chat.id, `Хорошо, А вы как?`);
+    }
+    if(message.text == 'МГУ') {
+        bot.sendMessage(msg.chat.id, `Хехе..`);
+    }
+    if(message.text == 'Прошай бот') {
+        bot.sendMessage(msg.chat.id, `Прошайте`);
+    }
 });
