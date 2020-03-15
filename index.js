fa6ecfa095adfe0ea97fe5092d5a2c789bd45381
VoIdJMp <59548686+VoIdJMp@users.noreply.github.com>
2020-03-15 15:27:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,17 +11,6 @@ bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
     var text = match[1];
     var time = match[2];
 
-    notes.push({ 'uid': userId, 'time': time, 'text': text });
-
     bot.sendMessage(userId, 'Я обязательно напомню:)');
 });
 
-setInterval(function(){
-    for (var i = 0; i < notes.length; i++) {
-    const curDate = new Date().getHours() + ':' + new Date().getMinutes();
-    if (notes[i]['time'] === curDate) {
-      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
-      notes.splice(i, 1);
-    }
-  }
-}, 1000);
