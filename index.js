9835097e27878fe43f86a1d267068fb26dd5ddc4
VoIdJMp <voidjmp@mail.ru>
2020-03-27 15:34:20 +0300

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = '823493917:AAGZ8k8geDfaWYZPOFffQAZaigwq8eQurcg';
+
+const bot  = new TelegramBot(TOKEN, {polling: true});
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name} `);
+});
